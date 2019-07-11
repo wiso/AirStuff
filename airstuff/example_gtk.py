97117384da1787c9224bebfceab176838520da49
Ruggero Turra <ruggero.turra@cern.ch>
2019-07-11 09:25:05 +0200

    gtk top working

--- airstuff/example_gtk.py
+++ airstuff/example_gtk.py
@@ -15,27 +15,31 @@ class MyWindow(Gtk.Window):
         Gtk.Window.__init__(self, title="Hello World")
 
         self.set_default_size(1000, 350)
+        main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.add(main_box)
 
         box_inspire = Gtk.Box()
-        self.add(box_inspire)
-
-        self.grid = Gtk.Grid()
-        self.add(self.grid)
+        main_box.add(box_inspire)
 
         self.button_start = Gtk.Button(label="start")
-        self.button_start.connect("clicked", self.run)
-        #self.grid.attach(self.button_start, 0, 1, 1, 1)
+        self.button_start.connect("clicked", self.run_stop_inspire)
+        self.inspire_running = False
         box_inspire.pack_start(self.button_start, True, True, 0)
-        self.button_stop = Gtk.Button(label="stop")
-        self.button_stop.connect("clicked", self.stop)
-        #self.grid.attach(self.button_stop, 1, 1, 1, 1)
-        box_inspire.pack_start(self.button_stop, True, True, 0)
+
+
+        box_table = Gtk.Box()
+        main_box.add(box_table)
 
         sw = Gtk.ScrolledWindow()
-        self.grid.attach(sw, 0, 2, 2, 1)
+        sw.set_policy(
+            Gtk.PolicyType.ALWAYS, Gtk.PolicyType.ALWAYS)
+        sw.set_size_request(1000, 200)
+        box_table.pack_start(sw, True, True, 0)
+
         self.table_store = Gtk.ListStore(str, str, str)
         self.table_view = Gtk.TreeView(model=self.table_store)
         sw.add(self.table_view)
+        self.table_view.set_search_column(1)
 
         headers = 'doi', 'title', 'date'
         for i in range(3):
@@ -44,10 +48,23 @@ class MyWindow(Gtk.Window):
 
         self.indico_query = None
 
-        self.frame_air = Gtk.Frame()
-        self.grid.attach(self.frame_air, 1, 3, 1, 1)
+    def run_stop_inspire(self, widget):
+        if self.inspire_running:
+            self.button_start.get_children()[0].set_label('stopping')
+            self.button_start.set_sensitive(False)
+            self.stop_inspire()
+            self.inspire_running = False
+            self.button_start.get_children()[0].set_label('start')
+            self.button_start.set_sensitive(True)
+        else:
+            self.button_start.get_children()[0].set_label('starting')
+            self.button_start.set_sensitive(False)
+            self.run_inspire()
+            self.inspire_running = True
+            self.button_start.get_children()[0].set_label('stop')
+            self.button_start.set_sensitive(True)
 
-    def run(self, widget):
+    def run_inspire(self):
         lock = Lock()
 
         def f(item):
@@ -57,7 +74,7 @@ class MyWindow(Gtk.Window):
         self.indico_query = inspire.IndicoQuery(callback=f)
         self.indico_query.run()
 
-    def stop(self, widget):
+    def stop_inspire(self):
         self.indico_query.stop()
         self.indico_query = None
 
@@ -65,8 +82,6 @@ class MyWindow(Gtk.Window):
         msg = "%s\n" % item
         item = inspire.fix_info(item)
         self.table_store.append([item['doi'], str(item['title']), str(item['creation_date'])])
-
-        print(msg)
 
 win = MyWindow()
 win.connect("destroy", Gtk.main_quit)
